50eedd9361c3ece979bb39c543fe6a5f6c22344a
Moonzera <jolucascosta05@gmail.com>
2026-03-16 10:51:10 -0300

    salvando código antes de adicionar TailwindCSS

--- fs_frontend/src/app/(auth)/signin.tsx
+++ fs_frontend/src/app/(auth)/signin.tsx
@@ -1,13 +1,45 @@
 import { useRouter } from "expo-router"
-import { Button, Text, View } from "react-native"
+import { Text, TextInput, TouchableOpacity, View } from "react-native"
 
 export default function SignIn() {
   const router = useRouter()
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text style={{ textAlign: "center" }}>Tela de Login</Text>
-      <Button title="Ir para Cadastro" onPress={() => router.push("/signup")} />
+    <View style={{ flex: 1, justifyContent: "center", padding: 20, backgroundColor: "#051F20"}}>
+      <Text style={{ fontSize: 48,  color: "#fff" }}>Login</Text>
+      <View style={{ flexDirection: "row", gap: 5, marginTop: 15}}>
+        <Text style={{ color: "#fff"}}>Ainda não tem uma Conta?</Text>
+        <TouchableOpacity onPress={() => router.push("/signup")}>
+          <Text style={{color: "#8EB69B"}}>Criar uma conta</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{marginVertical: 40}}>
+        <TextInput placeholder="Email" style={{ 
+          padding: 10, 
+          width: "auto", 
+          borderColor: "#DAF1DE", 
+          backgroundColor: "#8EB69B",
+          borderRadius: 10,
+          borderWidth: 2,
+          color: "#fff"
+        }}/>
+        <TextInput placeholder="Senha" style={{ 
+          padding: 10, 
+          width: "auto", 
+          borderColor: "#DAF1DE", 
+          backgroundColor: "#8EB69B",
+          borderRadius: 10,
+          borderWidth: 2,
+          color: "#fff",
+          marginTop: 20
+        }}/>
+      </View>
+      <View>
+        <Text>Esqueci a senha</Text>
+        <TouchableOpacity>
+          <Text style={{ color: "#fff"}}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- fs_frontend/src/app/(auth)/signup.tsx
+++ fs_frontend/src/app/(auth)/signup.tsx
@@ -5,7 +5,7 @@ export default function SignUp() {
   const router = useRouter()
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
+    <View style={{ flex: 1, justifyContent: "center"}}>
       <Text style={{ textAlign: "center" }}>Tela de Cadastro</Text>
       <Button title="Ir para login" onPress={() => router.push("/signin")} />
     </View>
