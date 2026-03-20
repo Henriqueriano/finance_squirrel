e59c1e51e22ec8b6bbf29d9026ff1ee54ad0e7a0
Daniel Henrique <daniel.neves0021@gmail.com>
2026-03-19 21:52:05 -0300

    feat: expense crud and dependecies

--- fs_backend/src/models.py
+++ fs_backend/src/models.py
@@ -10,17 +10,17 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 class Base(DeclarativeBase):
     pass
 
-class ExpensesCategory(Base):
+class ExpenseCategoryModel(Base):
     __tablename__ = 'expenses_categoryes_table'
 
     category_id: Mapped[int] = mapped_column(Integer, primary_key = True)
     category_name: Mapped[str] = mapped_column(String(50), nullable = False)
     user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users_table.user_id'))
     
-    expenses: Mapped['Expenses'] = relationship(back_populates = 'ExpensesCategory')
-    users: Mapped['Users'] = relationship(back_populates = 'ExpensesCategory')
+    expenses: Mapped['ExpenseModel'] = relationship(back_populates = 'categoryes')
+    users: Mapped['UserModel'] = relationship(back_populates = 'categoryes')
 
-class Expenses(Base):
+class ExpenseModel(Base):
     __tablename__ = 'expenses_table'
 
     expense_id: Mapped[int] = mapped_column(Integer, primary_key = True) 
@@ -30,15 +30,18 @@ class Expenses(Base):
                                                  nullable = False,
                                                  default= lambda: datetime.now(timezone.utc)) 
     expense_date: Mapped[datetime] = mapped_column(DateTime(timezone = True), nullable = True, default=None) 
-    category_id: Mapped[str] = mapped_column(ForeignKey('expenses_categoryes_table.category_id'))
+    category_id: Mapped[int] = mapped_column(ForeignKey('expenses_categoryes_table.category_id'))
     expense_desc: Mapped[str] = mapped_column(String(300), nullable = True, default=None)
     transaction_type: Mapped[bool] = mapped_column(default= False, nullable = True)
 
-    users: Mapped["Users"] = relationship(back_populates = 'Expenses')
+    users: Mapped['UserModel'] = relationship(back_populates = 'expenses')
+    categoryes: Mapped['ExpenseCategoryModel'] = relationship(back_populates = 'expenses')
 
-class Users(Base):
+class UserModel(Base):
     __tablename__ = 'users_table'
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid = True), primary_key = True, default = uuid.uuid7) 
     user_name: Mapped[str] = mapped_column(String(100), nullable = False)
+    
+    expenses: Mapped[List['ExpenseModel']] = relationship(back_populates = 'users')
+    categoryes: Mapped['ExpenseCategoryModel'] = relationship(back_populates = 'users')
 
-    expenses: Mapped[List["Expenses"]] = relationship(back_populates = 'Users')
