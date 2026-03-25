432a85ebba53d4562476075b579d558d7c1ab7a5
Daniel H. <daniel.neves0021@gmail.com>
2026-03-25 18:21:34 -0300

    feat: ended crud?

--- fs_backend/src/controllers.py
+++ fs_backend/src/controllers.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
 from .dtos import *
 from .services import *
+from fastapi import APIRouter, HTTPException, status
 router = APIRouter()
 
-## expenses section: 
+# region expenses: 
 @router.post('/expenses_bulk_register/')
 async def expenses_bulk_register(payload: list[ExpensesDto]) -> None:
     if payload == None:
@@ -51,22 +51,48 @@ async def delete_expense(expense_id: int) -> None:
         raise HTTPException(
                 status_code=500,
                 detail='server error')
+# end region
 
-
-## categoryes section:
-@router.post('/categoryes_bulk_register/')
-async def categoryes_bulk_register(payload: list[ExpensesCategoryDto]) -> None:
+# region categories:
+@router.post('/categories_register/')
+async def categories_register(payload: ExpensesCategoryDto) -> None:
     if payload == None:
         raise HTTPException(
                 status_code=400,
                 detail='payload cannot be None')
-    service_response = await categoryes_bulk_register_service(payload)
+    service_response = await categories_register_service(payload)
     if (not service_response):
         raise HTTPException(
                 status_code=500,
                 detail='server error')
 
-## user section:
+@router.patch('/update_category/')
+async def update_category(category_id: str, payload: ExpenseCategoryModel) -> None:
+    if payload is None:
+        raise HTTPException(
+                status_code=400,
+                detail='category_id cannot be None')
+    service_response = await update_category_service(category_id, payload)
+    if (not service_response):
+        raise HTTPException(
+                status_code=500,
+                detail='server error')
+
+@router.get('/get_all_categories/')
+async def get_all_categories(user_id: str) -> list[ExpensesCategoryDto]:
+    if user_id is None or user_id == '':
+        raise HTTPException(
+                status_code=400,
+                detail='user_id cannot be None')
+    service_response = await get_all_categories(user_id)
+    if (len(service_response) == 0):
+        raise HTTPException(
+                status_code=500,
+                detail='nothing on the base')
+
+# endregion
+
+# region user:
 @router.post('/register_user/')
 async def register_user(payload: UserDto) -> None:
     if payload == None:
@@ -78,5 +104,5 @@ async def register_user(payload: UserDto) -> None:
         raise HTTPException(
                 status_code=500,
                 detail='server error')
-
+# endregion
    
